08df8563f422c65af15477e1e6675e42fd4c3a59
Jeffrey Li <jeffreyli0211@gmail.com>
2020-10-30 12:01:24 -0400

    Moved weather.py back into prototype folder

--- prototype/weather.py
+++ prototype/weather.py
@@ -1,12 +1,13 @@
 import requests
 import json
+import os
 
 # Basic function to get the current weather of a user's location based on the inputs city and state
 def get_weather(city, state):
     url = "http://api.openweathermap.org/data/2.5/weather?"
     q = "q=" + city + "," + state                           # Set the string query parameters for the location of the user
     units = "&units=imperial"                               # Set the units of measurement (hard-coded as imperial, but this can be set to be by user preference)
-    key = "&appid=77d0aa9c2f5fa4e1c6a4e4e87d644e52"         # API key, do NOT hard-code API key in prototype
+    key = "&appid=" + os.getenv('WEATHER_API_KEY')          # Make sure to set the API key as an environment variable WEATHER_API_KEY within your system to avoid unncessary hard-coding of the keys used
     get = url + q + units + key                             # Create the full API call
     resp = requests.get(get).json()
 
