529abf2509ed77efe2c91e6c3360f5f18cc5a2da
Joseph Lai <josephl@bu.edu>
2020-12-10 22:13:47 -0500

    connected spotify api with web player

--- AM-InTuneWithTheWeather/app.py
+++ AM-InTuneWithTheWeather/app.py
@@ -1,35 +1,46 @@
-from flask import Flask, render_template,redirect,session
+from flask import Flask, render_template, redirect, session
 from functools import wraps
 import pymongo
 app = Flask(__name__)
 app.secret_key = "fkjerhshdjjkefjksdjhkfshjkdsjhksfdjjsadjhka"
 
-#Database
-client = pymongo.MongoClient('localhost', 27017)
+# Database
+# client = pymongo.MongoClient('localhost', 27017)
+client = pymongo.MongoClient("mongodb+srv://411projectteam:411projecteam@cluster0.94v8n.mongodb.net/user_login_system?retryWrites=true&w=majority")
 db = client.user_login_system
 
 # Decorators
+
+
 def login_required(f):
     @wraps(f)
     def wrap(*args, **kwargs):
         if 'logged_in' in session:
-            return f(*args,**kwargs)
+            return f(*args, **kwargs)
         else:
             return redirect('/')
     return wrap
 
+
 # Routes
 from user import routes
+from user.models import User
+from user.player import Player
+
+thePlayer = Player()
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
 
 @app.route('/dashboard/')
 @login_required
 def dashboard():
     return render_template('dashboard.html')
 
+
 @app.route('/player/')
 def player():
+    thePlayer.findPlaylist()
     return render_template('player.html')
--- AM-InTuneWithTheWeather/user/models.py
+++ AM-InTuneWithTheWeather/user/models.py
@@ -4,12 +4,11 @@ from app import db
 import uuid
 
 class User:
-
-    def start_session(self,user):
+    def start_session(self, user):
         del user['password']
         session['logged_in'] = True
-        session['user'] = user
-        return jsonify(user),200
+        session['user'] = user 
+        return jsonify(user), 200
 
     def signup(self):
         print(request.form)
@@ -47,69 +46,59 @@ class User:
 
         # Check for existing user with same email
         if db.users.find_one({"email": user['email']}):
-            return jsonify({"error":"Email address already in use"}),400
+            return jsonify({"error": "Email address already in use"}), 400
 
-        
         if db.users.insert_one(user):
             return self.start_session(user)
 
-        return jsonify({"error":"Signup failed"}), 400
+        return jsonify({"error": "Signup failed"}), 400
 
     def signout(self):
         session.clear()
         return redirect('/')
-    
+
     def login(self):
         user = db.users.find_one({
-            "email":request.form.get('email')
+            "email": request.form.get('email')
         })
 
-        if user and pbkdf2_sha256.verify(request.form.get('password'),user['password']):
+        if user and pbkdf2_sha256.verify(request.form.get('password'), user['password']):
             return self.start_session(user)
 
-        return jsonify({"error":"invalid login credentials"}), 401
-    
+        return jsonify({"error": "invalid login credentials"}), 401
+
     def makeMoodChanges(self):
         if db.users.update(
-        {'_id': session['user']['_id']}, {'$set': { 
-                'sunny': request.form.get('sunny'),
-                'raining':request.form.get('raining'),
-                'snowing':request.form.get('snowing'),
-                'foggy':request.form.get('foggy'),
-                'cloudy':request.form.get('cloudy')
-            }}):
-            session['user'] = db.users.find_one({"_id":session['user']['_id']})
-            return jsonify({"error":"Successfully updated"}), 0
-        return jsonify({"error":"Could not make mood changes"}), 399
+                {'_id': session['user']['_id']}, {'$set': {
+                    'sunny': request.form.get('sunny'),
+                    'raining': request.form.get('raining'),
+                    'snowing': request.form.get('snowing'),
+                    'foggy': request.form.get('foggy'),
+                    'cloudy': request.form.get('cloudy')
+                }}):
+            session['user'] = db.users.find_one(
+                {"_id": session['user']['_id']})
+            return jsonify({"error": "Successfully updated"}), 0
+        return jsonify({"error": "Could not make mood changes"}), 399
 
     def makeGenreChanges(self):
         if db.users.update(
-        {'_id': session['user']['_id']}, {'$set': { 
-            "hiphop": request.form.get('hiphop'),
-            "pop": request.form.get('pop'),
-            "country": request.form.get('country'),
-            "rock": request.form.get('rock'),
-            "latin": request.form.get('latin'),
-            "r&b": request.form.get('r&b'),
-            "indie": request.form.get('indie'),
-            "party": request.form.get('party'),
-            "classical": request.form.get('classical'),
-            "jazz": request.form.get('jazz'),
-            "folk&acoustic": request.form.get('folk&acoustic'),
-            "soul": request.form.get('soul'),
-            "metal": request.form.get('metal')
-            }}):
-            session['user'] = db.users.find_one({"_id":session['user']['_id']})
-            return jsonify({"error":"Successfully updated"}), 0
-        return jsonify({"error":"Could not make genre preference changes"}), 398
-    
-    # def player(self): joeseph when you incorproate the spotify(will need to import it as well into this file) user "session['user'][(insert here the mood or value you are looking for)]
-        #set play as a string var equal to whatever the playlist code is then uncomment below 
-        #
-        # if db.users.update(
-        # {'_id': session['user']['_id']}, {'$set': { 
-        #     "playlist": play
-        #     }}):
-#        return jsonify({"error":"Successfully updated"}), 0
- #      return jsonify({"error":"Could not create playlist"}), 398
-        # uncomment: session['user'] = db.users.find_one({"_id":session['user']['_id']})
+                {'_id': session['user']['_id']}, {'$set': {
+                    "hiphop": request.form.get('hiphop'),
+                    "pop": request.form.get('pop'),
+                    "country": request.form.get('country'),
+                    "rock": request.form.get('rock'),
+                    "latin": request.form.get('latin'),
+                    "r&b": request.form.get('r&b'),
+                    "indie": request.form.get('indie'),
+                    "party": request.form.get('party'),
+                    "classical": request.form.get('classical'),
+                    "jazz": request.form.get('jazz'),
+                    "folk&acoustic": request.form.get('folk&acoustic'),
+                    "soul": request.form.get('soul'),
+                    "metal": request.form.get('metal')
+                }}):
+            session['user'] = db.users.find_one(
+                {"_id": session['user']['_id']})
+            return jsonify({"error": "Successfully updated"}), 0
+        return jsonify({"error": "Could not make genre preference changes"}), 398
