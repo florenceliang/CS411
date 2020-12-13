3d17f7ccff3bc3dd62d5dfb50739cc5e4d85eb0d
Jeffrey Li <jeffreyli0211@gmail.com>
2020-12-13 14:06:35 -0500

    Debugging oauth

--- AM-InTuneWithTheWeather/static/js/scripts.js
+++ AM-InTuneWithTheWeather/static/js/scripts.js
@@ -40,27 +40,6 @@ $("form[name=login_form]").submit(function(e) {
     e.preventDefault();
 });
 
-$("form[name=google-signin]").submit(function(e) {
-
-    var $form = $(this);
-    var $error = $form.find(".error");
-    var data = $form.serialize();
-
-    $.ajax({
-        url: "/user/loginGoogle",
-        type: "POST",
-        data: data,
-        dataType: "json",
-        success: function(resp){
-            window.location.href = "/dashboard/";
-        },
-        error: function(resp){
-            $error.text(resp.responseJSON.error).removeClass("error--hidden");
-        }
-    });
-    e.preventDefault();
-});
-
 $("form[name=mood_form]").submit(function(e) {
 
     var $form = $(this);
