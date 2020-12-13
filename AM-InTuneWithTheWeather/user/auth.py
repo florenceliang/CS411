12104236dbdb5d34bf86bbeb0618a3ebd410173b
Jeffrey Li <jeffreyli0211@gmail.com>
2020-12-12 23:45:11 -0500

    Updated auth.py

--- AM-InTuneWithTheWeather/user/auth.py
+++ AM-InTuneWithTheWeather/user/auth.py
@@ -14,3 +14,22 @@ from user.models import User
 
 #blueprint = Blueprint('auth', __name__, url_base='/auth')
 
+GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID", None)
+GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET", None)
+
+client = WebApplicationClient(GOOGLE_CLIENT_ID)
+
+@app.route('/user/loginGoogle', methods = ['POST'])
+def loginGoogle():
+    # Google login URL that should be reached
+    auth_endpoint = "https://accounts.google.com/o/oauth2/v2/auth"
+    redirect_uri = request.base_url + "/callback"
+
+    # Set the scopes that our app will get from Google if the user
+    # consents on login page
+    request_uri = client.prepare_request_uri(
+        auth_endpoint,
+        redirect_uri = redirect_uri,
+        scope = ["email", "profile", "openid"]
+    )
+    return redirect(request_uri)
